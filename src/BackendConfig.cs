8ce525947ca9816f62631d03e8cc5b5afdfea5b2
andredewes <andredewes@microsoft.com>
2024-01-15 14:21:09 -0300

    Initial commit

---
+++ src/BackendConfig.cs
@@ -0,0 +1,66 @@
+﻿namespace openai_loadbalancer;
+
+public class BackendConfig
+{
+    public static int HttpTimeoutSeconds = 100;
+
+    public required string Url { get; set; }
+    public string? DeploymentName { get; set; }
+    public int Priority { get; set; }
+    public required string ApiKey { get; set; }
+
+    public static IReadOnlyDictionary<string, BackendConfig> LoadConfig(IConfiguration config)
+    {
+        var returnDictionary = new Dictionary<string, BackendConfig>();
+
+        var environmentVariables = config.AsEnumerable().Where(x => x.Key.ToUpperInvariant().StartsWith("BACKEND_")).ToDictionary(x => x.Key, x => x.Value, StringComparer.OrdinalIgnoreCase);
+
+        var numberOfBackends = environmentVariables.Select(x => x.Key.Split('_')[1]).Distinct();
+
+        if (environmentVariables.Count() == 0 || numberOfBackends.Count() == 0)
+        {
+            throw new Exception("Could not find any environment variable starting with 'BACKEND_[x]'... please define your backend endpoints");
+        }
+
+        foreach (var backendIndex in numberOfBackends)
+        {
+            var key = $"BACKEND_{backendIndex}";
+            var url = LoadEnvironmentVariable(environmentVariables, backendIndex, "URL");
+            var deploymentName = LoadEnvironmentVariable(environmentVariables, backendIndex, "DEPLOYMENT_NAME", isMandatory: false);
+            var apiKey = LoadEnvironmentVariable(environmentVariables, backendIndex, "APIKEY");
+            var priority = Convert.ToInt32(LoadEnvironmentVariable(environmentVariables, backendIndex, "PRIORITY"));
+
+            returnDictionary.Add(key, new BackendConfig { Url = url, ApiKey = apiKey, Priority = priority, DeploymentName = deploymentName });
+        }
+
+        //Load the general settings not in scope only for specific backends
+        var httpTimeout = Environment.GetEnvironmentVariable("HTTP_TIMEOUT_SECONDS");
+
+        if (httpTimeout != null)
+        {
+            HttpTimeoutSeconds = Convert.ToInt32(httpTimeout);
+        }
+
+        return returnDictionary;
+    }
+
+    private static string? LoadEnvironmentVariable(IDictionary<string, string?> variables, string backendIndex, string property, bool isMandatory = true)
+    {
+        var key = $"BACKEND_{backendIndex}_{property}";
+
+        if (!variables.TryGetValue(key, out var value) && isMandatory)
+        {
+            throw new Exception($"Missing environment variable {key}");
+        }
+
+        if (value != null)
+        {
+            return value.Trim();
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
+    
---
+++ src/YarpConfiguration.cs
@@ -0,0 +1,155 @@
+using System.Net;
+using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Forwarder;
+using Yarp.ReverseProxy.Health;
+using Yarp.ReverseProxy.Model;
+using Yarp.ReverseProxy.Transforms;
+
+namespace openai_loadbalancer;
+
+public class YarpConfiguration
+{
+    private readonly IReadOnlyDictionary<string, BackendConfig> backends;
+
+    public YarpConfiguration(IReadOnlyDictionary<string, BackendConfig> backends)
+    {
+        this.backends = backends;
+    }
+
+    public RouteConfig[] GetRoutes()
+    {
+        return
+        [
+                new RouteConfig()
+                {
+                    RouteId = "route",
+                    ClusterId = "cluster",
+                    Match = new RouteMatch
+                    {
+                        Path = "{**catch-all}"
+                    }
+                }
+            ];
+    }
+    public ClusterConfig[] GetClusters()
+    {
+
+        var destinations = new Dictionary<string, DestinationConfig>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var backend in backends)
+        {
+            var metadata = new Dictionary<string, string>
+            {
+                { "url", backend.Value.Url },
+                { "priority", backend.Value.Priority.ToString() }
+            };
+            destinations.Add(backend.Key, new DestinationConfig { Address = backend.Value.Url, Metadata = metadata });
+        }
+
+        return
+        [
+                new ClusterConfig()
+                {
+                    ClusterId = "cluster",
+                    HealthCheck = new HealthCheckConfig
+                    {
+                        Passive = new PassiveHealthCheckConfig
+                        {
+                            Enabled = true,
+                            Policy = ThrottlingHealthPolicy.ThrottlingPolicyName,
+                        }
+                    },
+                    Destinations = destinations,
+                    HttpRequest = new ForwarderRequestConfig { ActivityTimeout = TimeSpan.FromSeconds(BackendConfig.HttpTimeoutSeconds) }
+                }
+        ];
+    }
+
+    public Func<ResponseTransformContext, ValueTask> TransformResponse()
+    {
+        return context =>
+        {
+            if (context.ProxyResponse?.StatusCode is HttpStatusCode.TooManyRequests or >= HttpStatusCode.InternalServerError)
+            {
+                var reverseProxyContext = context.HttpContext.GetReverseProxyFeature();
+
+                var canRetry = reverseProxyContext.AllDestinations.Count(m => m.Health.Passive != DestinationHealth.Unhealthy && m.DestinationId != reverseProxyContext?.ProxiedDestination?.DestinationId) > 0;
+
+                if (canRetry)
+                {
+                    // Suppress the response body from being written when we will retry
+                    context.SuppressResponseBody = true;
+                }
+            }
+
+            return default;
+        };
+    }
+
+    internal Func<RequestTransformContext, ValueTask> TransformRequest()
+    {
+        return context =>
+        {
+            var proxyHeaders = context.ProxyRequest.Headers;
+            var reverseProxyFeature = context.HttpContext.GetReverseProxyFeature();
+
+            var backendConfig = backends[reverseProxyFeature.AvailableDestinations[0].DestinationId];
+            proxyHeaders.Remove("api-key");
+            proxyHeaders.Add("api-key", backendConfig.ApiKey);
+
+            if (backendConfig.DeploymentName != null)
+            {
+                var pathSegments = context.Path.Value!.Split('/');
+
+                if (pathSegments.Length >= 4)
+                {
+                    //Incoming path should be coming in format "/openai/deployments/{deploymentName}/*"
+                    //We must grab the {deploynameName} from in the incoming request (array position [3]) and replace it by the one specified in the configuration
+                    context.Path = new PathString(context.Path.Value.Replace(pathSegments[3], backendConfig.DeploymentName));
+                }
+            }
+            
+            return default;
+        };
+    }
+}
+
+public class ThrottlingHealthPolicy : IPassiveHealthCheckPolicy
+{
+    public static string ThrottlingPolicyName = "ThrottlingPolicy";
+    private readonly IDestinationHealthUpdater _healthUpdater;
+
+    public ThrottlingHealthPolicy(IDestinationHealthUpdater healthUpdater)
+    {
+        _healthUpdater = healthUpdater;
+    }
+
+    public string Name => ThrottlingPolicyName;
+
+    public void RequestProxied(HttpContext context, ClusterState cluster, DestinationState destination)
+    {
+        var headers = context.Response.Headers;
+
+        if (context.Response.StatusCode is 429 or >= 500)
+        {
+            var retryAfterSeconds = 10;
+
+            if (headers.TryGetValue("Retry-After", out var retryAfterHeader) && retryAfterHeader.Count > 0 && int.TryParse(retryAfterHeader[0], out var retryAfter))
+            {
+                retryAfterSeconds = retryAfter;
+            }
+            else
+            if (headers.TryGetValue("x-ratelimit-reset-requests", out var ratelimiResetRequests) && ratelimiResetRequests.Count > 0 && int.TryParse(ratelimiResetRequests[0], out var ratelimiResetRequest))
+            {
+                retryAfterSeconds = ratelimiResetRequest;
+            }
+            else
+            if (headers.TryGetValue("x-ratelimit-reset-tokens", out var ratelimitResetTokens) && ratelimitResetTokens.Count > 0 && int.TryParse(ratelimitResetTokens[0], out var ratelimitResetToken))
+            {
+                retryAfterSeconds = ratelimitResetToken;
+            }
+
+            _healthUpdater.SetPassive(cluster, destination, DestinationHealth.Unhealthy, TimeSpan.FromSeconds(retryAfterSeconds));
+        }
+    }
+}
